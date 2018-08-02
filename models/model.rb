2fce73f8ed388b140612c4289bf92a2ad1b734ac
Jennifer Okwara <jokwara03@gmail.com>
2018-08-02 14:51:26 +0000

    automatically backed up by learn

---
+++ models/model.rb
@@ -0,0 +1,35 @@
+# def feminism_quiz(question1)
+  
+#   if question1 == "answer2"
+#     return "Correct! Awesome job!"
+#   else
+#     return "It's okay to be wrong! You are not the only one."
+#   end 
+
+# end
+
+@@results = []
+
+def feminism_quiz_question1(answer)
+  if answer == "answer2"
+    @@results.push "AWesome"
+  else 
+    @@results.push "oops"
+  end 
+end 
+
+def feminism_quiz_question2(answer)
+  if answer == "answer3"
+    @@results.push "AWesome"
+  else 
+    @@results.push "oops"
+  end 
+end 
+
+def feminism_quiz_question3(answer)
+  if answer == "answer1"
+    @@results.push "AWesome"
+  else 
+    @@results.push "oops"
+  end 
+end 
