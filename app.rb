33bac10c71770be5c5e340ec60bff643a8d894c6
Jennifer Okwara <jokwara03@gmail.com>
2018-08-02 14:53:54 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require_relative 'config/environment'
+require_relative 'models/model.rb' 
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    erb :feminism_form
   end
 
-end
+  post '/' do
+    @myresult = feminism_quiz_question1(params[:question1])
+    @myresult = feminism_quiz_question2(params[:question2])
+    @myresult = feminism_quiz_question3(params[:question1])
+    erb :results
+  end 
+end 
+  # Add your post route and action below
